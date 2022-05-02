760f7f400eb43043b828a51bc00887d81c43481a
huynl <huy.nguyenle@elinext.com>
2022-05-02 16:31:28 +0700

    Update code

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
-use App\Models\Advertisement;
 use App\Models\Category;
-use Illuminate\Database\Eloquent\Builder;
 
 class HomeController extends Controller
 {
@@ -16,7 +14,6 @@ class HomeController extends Controller
 
     public function index()
     {
-        $ads = Advertisement::query()->take(8)->get();
         $rootCategories = Category::query()->rootParent()->with('advertisementsRoot', function ($builder) {
             $builder->latest()->take(8);
         })->get();
